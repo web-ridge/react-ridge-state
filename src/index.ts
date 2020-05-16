0c0380c41077d672317935fbba56390eca8f4fec
Richard Lindhout <richardlindhout96@gmail.com>
2020-05-16 14:32:59 +0200

    Try to remove proxy dependency

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { useEffect, useState, useCallback } from "react";
 
 // global reference to store things in React (Native)
 // @ts-ignore
-const root: any = window || global;
+const r: any = window || global;
 
 interface StateWithValue<T> {
-  value: T;
+  internal: {
+    value: T;
+  };
+  sk: string;
+  _set: (n: T) => any;
 }
 
 export function newRidgeState<T>({
@@ -15,41 +19,29 @@ export function newRidgeState<T>({
   key: string;
   defaultState: T;
 }): StateWithValue<T> {
-  const subscriberKey = `wr_subscribers_${key}`;
+  // subscriber key
+  const sk = `wr_subscribers_${key}`;
+  r[sk] = [];
 
-  root[subscriberKey] = [];
-
-  const proxy = new Proxy(
-    { value: defaultState },
-    {
-      getOwnPropertyDescriptor(target: any, prop: any) {
-        if (prop === "subscriberKey") {
-          return { configurable: true, enumerable: true, value: subscriberKey };
-        }
-        return undefined;
-      },
-      set(state: StateWithValue<T>, _: string, newState: T) {
-        // This is a proxy hook
-        // We only want to change value so we ignore the key here
-        state.value = newState;
-
-        // let subscribers know value did change
-        root[subscriberKey].forEach((callback: any) => callback(newState));
-        return true;
-      },
-    }
-  );
-
-  return proxy as any;
+  const internal = { value: defaultState };
+  return {
+    internal,
+    sk,
+    _set: (newState: T) => {
+      internal.value = newState;
+      // let subscribers know value did change
+      r[sk].forEach((callback: any) => callback(newState));
+    },
+  };
 }
 
-export function useRidgeState<T>(state: {
-  value: T;
-}): [T, (newState: T) => any] {
-  const [localState, setLocalState] = useState<T>(state.value);
+export function useRidgeState<T>(
+  state: StateWithValue<T>
+): [T, (newState: T) => any] {
+  const [localState, setLocalState] = useState<T>(state.internal.value);
 
   const updateLocalStateIfDifferent = useCallback(
-    (newState) => {
+    (newState: T) => {
       if (newState !== localState) {
         setLocalState(newState);
       }
@@ -65,40 +57,29 @@ export function useRidgeState<T>(state: {
     }
 
     // @ts-ignore
-    const subscriberKey = Object.getOwnPropertyDescriptor(
-      state,
-      "subscriberKey"
-    ).value;
-
-    // @ts-ignore
-    window[subscriberKey].push(stateChanged);
+    const subscriberKey = state.sk;
+    r[subscriberKey].push(stateChanged);
     return () => {
       // @ts-ignore
-      window[subscriberKey] = window[subscriberKey].filter(
+      r[subscriberKey] = r[subscriberKey].filter(
         (f: () => any) => f !== stateChanged
       );
     };
   });
 
-  const setter = useCallback(
-    (newState: T) => {
-      // change local state as fast as possible
-      setLocalState(newState);
-
-      // update proxy value
-      // the proxy will let subscribers know it has changed
-      state.value = newState;
-    },
-    [state.value]
-  );
+  const setter = useCallback((newState: T) => {
+    // change local state as fast as possible
+    setLocalState(newState);
+    state._set(newState);
+  }, []);
 
   return [localState, setter];
 }
 
 export function getRidgeState<T>(state: StateWithValue<T>): T {
-  return state.value;
+  return state.internal.value;
 }
 
 export function setRidgeState<T>(state: StateWithValue<T>, newState: T) {
-  state.value = newState;
+  state._set(newState);
 }
