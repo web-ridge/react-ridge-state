fcb83f7468e120791fa578219aad4334076c0f9d
Gerald <me@jacobford.co.uk>
2021-08-10 10:26:35 +0100

    feat: build cjs and esm

---
+++ dist/esm/e.js
@@ -0,0 +1,1 @@
+import*as e from"react";var t=typeof window!="undefined"&&typeof window.document!="undefined"&&typeof window.document.createElement!="undefined"?e.useLayoutEffect:e.useEffect;export{t as default};
---
+++ dist/esm/e.native.js
@@ -0,0 +1,1 @@
+import*as t from"react";var e=t.useLayoutEffect;export{e as default};
