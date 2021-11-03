1cdcea2eb49db278a0cac6217e4f3fc7f28cb477
Aarni Koskela <akx@iki.fi>
2021-11-03 10:26:05 +0200

    Humanize names for external interface

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export interface StateWithValue<T> {
   ) => TSelected;
   set: (
     newState: T | ((prev: T) => T), // can be the newState or a function with prevState in params and which needs to return new state
-    ac?: (newState: T) => void, // callback with the newState after state has been set
+    callback?: (newState: T) => void, // callback with the newState after state has been set
     ca?: (ns: T) => void // caller is used inside react components so we can we do faster updates to the caller
   ) => void;
   reset: () => void;
@@ -59,18 +59,21 @@ function useComparator<T>(v: T, c: Comparator<T> = equ): T {
   return nv;
 }
 
-export function newRidgeState<T>(iv: T, o?: Options<T>): StateWithValue<T> {
+export function newRidgeState<T>(
+  initialValue: T,
+  options?: Options<T>
+): StateWithValue<T> {
   // subscribers with callbacks for external updates
   let sb: SubscriberFunc<T>[] = [];
 
   // internal value of the state
-  let v: T = iv;
+  let v: T = initialValue;
 
   // set function
-  function set(ns: T | ((prev: T) => T), ac?: (ns: T) => void) {
+  function set(newValue: T | ((prev: T) => T), callback?: (ns: T) => void) {
     const pv = v;
     // support previous as argument to new value
-    v = ns instanceof Function ? ns(v) : ns;
+    v = newValue instanceof Function ? newValue(v) : newValue;
 
     // let subscribers know value did change async
     setTimeout(() => {
@@ -78,24 +81,24 @@ export function newRidgeState<T>(iv: T, o?: Options<T>): StateWithValue<T> {
       sb.forEach((c) => c(v));
 
       // callback after state is set
-      ac && ac(v);
+      callback && callback(v);
 
       // let options function know when state has been set
-      o && o.onSet && o.onSet(v, pv);
+      options && options.onSet && options.onSet(v, pv);
     });
   }
 
   // subscribe hook
-  function sub(c: SubscriberFunc<T>) {
+  function useSubscription(subscriber: SubscriberFunc<T>) {
     // subscribe effect
     useIsomorphicLayoutEffect(() => {
       // update local state only if it has not changed already
       // so this state will be updated if it was called outside of this hook
-      sb.push(c);
+      sb.push(subscriber);
       return () => {
-        sb = sb.filter((f) => f !== c);
+        sb = sb.filter((f) => f !== subscriber);
       };
-    }, [c]);
+    }, [subscriber]);
   }
 
   // use hook
@@ -103,15 +106,16 @@ export function newRidgeState<T>(iv: T, o?: Options<T>): StateWithValue<T> {
     T,
     (
       newState: T | ((prev: T) => T),
-      ac?: (newState: T) => void,
-      ca?: (ns: T) => void
+      callback?: (newState: T) => any,
+      ca?: (ns: T) => any
     ) => void
   ] {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [l, s] = useState<T>(v);
 
     // subscribe to external changes
-    sub(s);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useSubscription(s);
 
     // set callback
     return [l, set];
@@ -119,11 +123,11 @@ export function newRidgeState<T>(iv: T, o?: Options<T>): StateWithValue<T> {
 
   // select hook
   function useSelector<TSelected = unknown>(
-    se: (state: T) => TSelected,
-    eq: Comparator<TSelected> = equ
+    selector: (state: T) => TSelected,
+    comparator: Comparator<TSelected> = equ
   ): TSelected {
     const [rv] = use();
-    return useComparator(se(rv), eq);
+    return useComparator(selector(rv), comparator);
   }
 
   return {
@@ -132,6 +136,6 @@ export function newRidgeState<T>(iv: T, o?: Options<T>): StateWithValue<T> {
     useValue: () => use()[0],
     get: () => v,
     set,
-    reset: () => set(iv),
+    reset: () => set(initialValue),
   };
 }
